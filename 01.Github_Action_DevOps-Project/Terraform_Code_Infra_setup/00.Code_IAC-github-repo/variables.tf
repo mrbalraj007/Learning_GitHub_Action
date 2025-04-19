3a41266b296f280f90d2076d2b945402c9ab99e9
Balraj <balraj@example.com>
2025-04-19 20:43:43 +1000

    Created a new project

---
+++ 01.Github_Action_DevOps-Project/Terraform_Code_Infra_setup/00.Code_IAC-github-repo/variables.tf
@@ -0,0 +1,39 @@
+variable "repository_name" {
+  description = "The name of the GitHub repository"
+  type        = string
+}
+
+variable "repository_description" {
+  description = "A brief description of the GitHub repository"
+  type        = string
+}
+
+variable "repository_private" {
+  description = "Specifies whether the repository is private or public"
+  type        = bool
+  default     = false
+}
+
+variable "github_token" {
+  description = "GitHub personal access token"
+  type        = string
+  sensitive   = true
+  default     = "xxxxxxxxxxxxxxxxxxxxxxx"  # Define here Github Token value
+}
+
+variable "github_owner" {
+  description = "The GitHub owner (username or organization)"
+  type        = string
+}
+
+variable "actions_secrets" {
+  description = "Map of GitHub Actions secrets to set at the repository level"
+  type        = map(string)
+  default     = {}
+}
+
+variable "actions_variables" {
+  description = "Map of GitHub Actions variables to set at the repository level"
+  type        = map(string)
+  default     = {}
+}
